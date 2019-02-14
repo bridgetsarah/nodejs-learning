791e604b6dfe462e86189d8cb9f0631cd5435edd
Bridget Sarah <msbskelly@gmail.com>
2019-02-14 11:48:35 +0000

    Issues with Watcher-Argv

--- Books Following/Node JS - The right way - Jim Wilson/watcher-argv.js
+++ Books Following/Node JS - The right way - Jim Wilson/watcher-argv.js
@@ -1,10 +1,10 @@
-const
-    fs = require('fs'),
-    filename = progress.argv[2];
-if (!filename) {
-    throw Error("A file to watch must be specifified!");
-}    
-fs.watch(filename, function() {
-    console.log("File" + filename + "just changed!");
-});
-console.log("Now watching" + filename + "for changes");
+const fs = require('fs');
+const filename = process.argv[2];
+
+fs.watch('target.txt', (eventType, filename) =>{
+     if (!filename) {
+        throw Error('A file to watch must be specified!');
+    } else 
+        console.log (`Now watching ${filename} for changes...`);{
+
+        })
